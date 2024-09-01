d8fd42f45c4b3d7fcb1f5c88b9ac61cd4b40f42e
Diego Alfaro Baez <diego.alfaro.b@gmail.com>
2024-09-01 17:38:27 -0400

    entrega

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import Login from "./components/Login"
+import Register from "./components/Register"
+import Cart from "./components/Cart"
+import Pizza from "./components/Pizza"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      {/* <Home /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      {/* <Cart /> */}
+      <Pizza />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Pizza.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect,useState } from 'react'
+import precioChileno from '../utils/utils.js'
+
+const urlPizza = 'http://localhost:5000/api/pizzas/p001'
+
+const Pizza = () => {
+  const [pizza,setPizza] = useState ([])
+
+  const getPizza = async () => {
+    const response = await fetch(urlPizza)
+    const datos = await response.json()
+    console.log(datos)
+    setPizza(datos)
+  }
+
+  useEffect(() => {
+    getPizza()
+  },[])
+
+  const {img,name,desc,ingredients,price} = pizza
+
+  return (
+    <div className='contenedorMayor'>
+      <div className='contenedorUnaPizza'>
+        <div className='fotoPizzaContainer2'>
+          <img src = {img} alt={name} />
+        </div>
+        <div className='descripcionPizza'>
+          <div>
+            <h1>Pizza {name}</h1>
+            <p>{desc}</p>
+            <ul>
+              {ingredients.map((item,i)=>(<li key={i}>🍕 {item}</li>))}
+            </ul>
+          </div>
+          <div className='contenedorPrecio'>
+            <div className='botonContainer2'><h3>{precioChileno(price)}</h3></div>
+            <div className='botonContainer2'><div className='botonCompra'>Añadir 🛒</div></div>
+          </div>
+        </div>        
+      </div>
+    </div>
+  )
+}
+
+export default Pizza
